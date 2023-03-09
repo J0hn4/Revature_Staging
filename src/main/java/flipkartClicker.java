04ae6980259a0518e52e6434641eaee0b6a77b76
The Professor <prof_x22@protonmail.com>
2023-03-09 16:41:03 -0500

    working version 3

--- src/main/java/flipkartClicker.java
+++ src/main/java/flipkartClicker.java
@@ -31,7 +31,7 @@ public class flipkartClicker {
                         .presenceOfElementLocated(By.xpath(
                                 "/html/body/div[2]/div/div/button")));
 
-//        WebElement exitModal = driver.findElement(By.xpath("/html/body/div[2]/div/div/button"));
+
         exitModal.click();
 
         WebElement searchBar = driver.findElement(By.xpath("//body/div[@id='container']/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[1]/div[1]/input[1]"));
@@ -41,40 +41,30 @@ public class flipkartClicker {
         WebElement searchSubmit = driver.findElement(By.xpath("//body/div[@id='container']/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[1]/button[1]"));
 
         searchSubmit.click();
-//        searchSubmit.click();
+
 
         WebElement firstProduct = (new WebDriverWait(driver,Duration.ofSeconds(10)))
                 .until(ExpectedConditions
                         .presenceOfElementLocated(By.xpath(
                                 "//div[contains(text(),'APPLE iPhone 13 (Starlight, 128 GB)')]")));
 
-//        Thread.sleep(2000);
-//        WebElement memoryInfo = driver.findElement(By.xpath("//body/div[@id='container']/div[1]/div[3]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a[1]/div[2]/div[1]/div[3]/ul[1]/li[1]"));
-//        String memoryText = memoryInfo.getText();
 
-//        WebElement firstProduct = driver.findElement(By.xpath("//div[contains(text(),'APPLE iPhone 13 (Starlight, 128 GB)')]"));
         firstProduct.click();
 
-//        Thread.sleep(2000);
+
 
         ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
         driver.switchTo().window(tabs2.get(1));
 
 
-//        Thread.sleep(1000);
 
-//        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable());
-
-//        FluentWait<WebDriver> wait1 = new FluentWait<WebDriver>(driver).pollingEvery(1000).withTimeout(2, TimeUnit.SECONDS);
 
         WebElement addCart = (new WebDriverWait(driver,Duration.ofSeconds(10)))
                 .until(ExpectedConditions
                         .presenceOfElementLocated(By.xpath(
                                 "//body/div[@id='container']/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/button[1]")));
 
-//        new FluentWait<WebDriver>(driver).withTimeout(Duration.ofMillis(6000)).ignoring(NoSuchElementException.class).until(ExpectedConditions.visibilityOf(ByXPath,"//body/div[@id='container']/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/button[1]"));
 
-//        WebElement addCart = driver.findElement(By.xpath("//body/div[@id='container']/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/button[1]"));
         addCart.click();
 
         WebElement cart = (new WebDriverWait(driver,Duration.ofSeconds(10)))
@@ -82,11 +72,6 @@ public class flipkartClicker {
                         .presenceOfElementLocated(By.xpath(
                                 "//*[@id=\"container\"]/div/div[1]/div[1]/div[2]/div[6]/div/div/a")));
 
-        Thread.sleep(3000);
-
-
-
-//        WebElement cart = driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[6]/div[1]/div[1]/a[1]/span[1]"));
 
         cart.click();
 
@@ -96,9 +81,6 @@ public class flipkartClicker {
                                 "//span[contains(text(),'Place Order')]")));
 
 
-
-//        WebElement placeOrder = driver.findElement(By.xpath(""));
-
         placeOrder.click();
 
         WebElement login = driver.findElement(By.xpath("//body/div[@id='container']/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]"));
@@ -107,21 +89,6 @@ public class flipkartClicker {
 
         WebElement continueButton = driver.findElement(By.xpath("//body/div[@id='container']/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[3]/button[1]"));
         continueButton.click();
-
-        WebElement loginPath = driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[4]/button[1]/span[1]"));
-
-//        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
-
-        driver.switchTo().newWindow(WindowType.TAB);
-
-//        # Open a new window
-//        driver.execute_script("window.open('');");
-
-//# Switch to the new window and open new URL
-//        driver.switchTo().window(driver.getWindowHandle()[1]);
-//        driver.get(new_url)
-
-
 
         driver.quit();
 
